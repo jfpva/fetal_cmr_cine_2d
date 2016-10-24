869bd50cd68813f41f71c2bc3c5fc19d402a518b
Joshua van Amerom <joshua.vanamerom@kcl.ac.uk>
2016-10-24 14:50:40 +0100

    updated image sequence evaluation metric code

--- cine/eval/calc_image_metric.m
+++ cine/eval/calc_image_metric.m
@@ -16,14 +16,29 @@ get_mask_values_3d  = @( im, msk ) reshape( get_mask_values_vec( im, msk ), sum(
 
 % {
 
+disp('entropy')
+
 s = get_mask_values_vec( im, mask );
 
-b = sqrt( sum( get_mask_values_vec( im, mask ).^2, 1 ) );
-    % b = mean( sqrt( sum( get_mask_values_3d( im, mask ).^2, 1 ) ) );
+% b = mean( sqrt( sum( get_mask_values_3d( im, mask ).^2, 1 ) ) );
+    % NOTE: normalisation in usual entropy calculation would be done as 
+    b = sqrt( sum( get_mask_values_vec( im, mask ).^2, 1 ) );
+    % however, taking mean b over the total frames provided, yields an
+    % entropy value that is independent of number of frames, 
+    % BUT, this doesn't matter if all cines compared are same ROI size and
+    % same number of frames, AND entropy is compared between cases
+    % normalised by entropy of X[0] or mean(Y,time)
 
 p = bsxfun( @rdivide, s, b );
 
-h = - mean( p(:) .* log( p(:) ) );
+% h = - mean( p(:) .* log( p(:) ) );
+    % NOTE: again, using h = mean(p*log(p)) instead of 
+    h = - sum( p(:) .* log( p(:) ) );
+    % gives an entropy value that is independent of number of voxels in
+    % mask and number of frames, allowing for comparison between cases,
+    % BUT, this doesn't matter if all cines compared are same ROI size and
+    % same number of frames, AND entropy is compared between cases
+    % normalised by entropy of X[0] or mean(Y,time)
 
 metricVal = h;
 
@@ -34,7 +49,7 @@ metricVal = h;
 
 %{
 
-metricVal = entropy( get_mask_values_vec( im, mask ) ); 
+metricVal = 1/entropy( get_mask_values_vec( im, mask ) ); 
 
 %}
 
@@ -66,13 +81,15 @@ metricVal = calc_time_entropy( im, mask, ref );
 % v2
 %{
 
+disp('image entropy')
+
 s = get_mask_values_3d( im, mask );
 
 b = sqrt( sum( get_mask_values_3d( im, mask ).^2, 1 ) );
 
 p = bsxfun( @rdivide, s, b );
 
-h = sum( ( b / sum( b, 3 ) ) .* - sum( p .* log( p ), 1 ), 3 ) / numel( s );
+h = sum( ( b / sum( b, 3 ) ) .* - sum( p .* log( p ), 1 ), 3 ); % / numel( s );
 
 metricVal = h;
 
@@ -88,7 +105,7 @@ dy = 1.25;
 millimetrePerRr = 1e-1*mean([dx,dy])*2*sqrt(sum(mask(:))/pi);
 
 [g,gx,gy] = calc_imseq_gradient( im,  dx, dy, millimetrePerRr * t, millimetrePerRr * rr );
-% g = sqrt( gx.^2 + gy.^2 );
+g = sqrt( gx.^2 + gy.^2 );
 
 calc_max_val = @( r, msk ) mean( sum( get_mask_values_3d( r, msk ), 1 ), 3 );  % calc_max_val = @( r, msk ) sqrt( sum( get_mask_values_vec( r, msk ).^2 ) );
 
@@ -97,11 +114,11 @@ p = g / calc_max_val( g, mask );
 calc_grad_entropy = @( p, msk ) - mean( get_mask_values_vec( p, msk ) .* log2( get_mask_values_vec( p, msk ) ) );
 
 metricVal = calc_grad_entropy ( p, mask );
+
 %}
 
+
 end  % calc_image_metric(...)
-
-
 
 
 
@@ -181,4 +198,43 @@ end
 MetricValue = -nansum(PixelEntropies.*PixelWeights,3);
 end
 
-end  % imagemetric()
+end  % imagemetric(...)
+
+
+
+function [g,gx,gy,gt] = calc_imseq_gradient( imseq, x, y, t, rrInterval )
+
+x = reshape( x, [], 1, 1 );
+y = reshape( y, 1, [], 1 );
+t = reshape( t, 1, 1, [] );
+
+if numel( x ) == 1,
+    dx = x;
+else
+    dx = diff( x, 1, 1 ); 
+    dx(end+1) = dx(end);
+end
+
+if numel( y ) == 1,
+    dy = y;
+else
+    dy = diff( y, 1, 2 ); 
+    dy(end+1) = dy(end);
+end
+
+if numel( t ) == 1,
+    dt = t;
+else
+    dt = diff( cat( 3, t, t(1) + rrInterval ), 1, 3 ); 
+end
+
+gx = bsxfun( @rdivide, diff( padarray( imseq, [1,0,0], 'replicate', 'post'), 1, 1 ), dx );
+
+gy = bsxfun( @rdivide, diff( padarray( imseq, [0,1,0], 'replicate', 'post'), 1, 2 ), dy );
+
+gt = bsxfun( @rdivide, diff( padarray( imseq, [0,0,1], 'circular',  'post'), 1, 3 ), dt );
+
+g = sqrt( abs(gx).^2 + abs(gy).^2 + abs(gt).^2 );
+
+
+end  % calc_imseq_gradient(...)
